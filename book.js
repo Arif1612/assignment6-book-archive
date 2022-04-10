fd4d2e24f08fdf5c5d288a1b299e9c72f8d4cfe7
Md. Arif-Ul-Islam <arifulislam161148@gmail.com>
2022-04-10 16:50:21 +0600

    I can search book successfully

---
+++ book.js
@@ -0,0 +1,61 @@
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = ''
+    // console.log(searchText);
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        // .then(data => console.log(data.docs
+        // ))
+        .then(data => displaySearchResult(data.docs))
+
+}
+
+const displaySearchResult = books => {
+    // console.log(books);
+    const showBookDiv = document.getElementById('show-books');
+    //  clear previous search result
+    showBookDiv.textContent = '';
+    books.forEach(book => {
+
+        const makeDiv = document.createElement('div');
+
+        if (book.title.length === 0) {
+
+            makeDiv.innerHTML = `
+                <p>No Data Found</p>
+`
+            showBookDiv.appendChild(makeDiv);
+        }
+
+        else {
+
+            makeDiv.innerHTML = `
+            <p>Book Name: ${book.title}</p>
+            <p>Author: ${book.author_name}</p>
+            <p>First Publish: ${book.publish_date[0]}</p>
+            <p>Data Found Total: ${book.title.length}</p>
+            <br>
+            `
+            showBookDiv.appendChild(makeDiv);
+        }
+
+
+
+        // console.log(book);
+        // console.log(book.title);
+        // console.log(book.author_name);
+        // console.log(book.publish_date[0]);
+        // console.log(book.length);
+        // console.log();
+        //    <p>First Publish: ${book.publish_date[0]}</p>
+    })
+
+}
+
+
+
+
